d0a8eebe8ae7b227e35c21016f684f5ba96cd8f8
Chi-Chi-Chi-Chita <chita0901@gmail.com>
2021-05-19 18:20:02 +0800

    新增"請款"頁面；新增"請款組別"元件

--- resources/js/pages/finance/finance.vue
+++ resources/js/pages/finance/finance.vue
@@ -1,5 +1,84 @@
 <template>
-  <div>123</div>
+  <div class="container-fluid">
+    <!-- memu -->
+    <div class="d-flex mb-4 row align-items-center">
+      <div class="col-auto btn-group btn-group-lg d-flex mr-auto">
+        <button type="button" class="btn btn-primary align-self-center">請款</button>
+        <button type="button" class="btn btn-primary">設定</button>
+        <button type="button" class="btn btn-primary">匯出</button>
+      </div>
+      <form class="col-auto form-inline">
+        <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style="font-size:1.2rem">年份：</label>
+        <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref" style="font-size:1.2rem" v-model="filter.year">
+          <option :value="year" v-for="year in yearList" :key="`filter-year-${year}`">{{year}}</option>
+        </select>
+      </form>
+    </div>
+    <!--資訊欄-->
+    <div class="info d-flex align-items-center justify-content-between mb-3">
+      <div class="card ">
+        <h4 class="card-title">己入帳贊助款</h4>
+        <p class="card-text">123</p>
+      </div>
+      <div class="card mx-3">
+        <h4 class="card-title">本年度總預算</h4>
+        <p class="card-text">123</p>
+      </div>
+      <div class="card border-success">
+        <h4 class="card-title">本年度總支出</h4>
+        <p class="card-text">123</p>
+      </div>
+    </div>
+    <!--Content-->
+    <div class="row">
+      <div class="col-sm-1">
+        <FinanceSidebar :filterYear="filter.year" />
+      </div>
+      <div class="clo-sm-11">
+      </div>
+    </div>
+  </div>
 </template>
-<script></script>
-<style scoped lang="scss"></style>
+
+<script>
+  import FinanceSidebar from "./financeSidebar"
+  export default{
+    components: {
+      FinanceSidebar,
+    },
+    data(){
+      return{
+        yearList:['2022','2021','2019'],
+        filter:{
+          year:2021,
+        }
+      }
+    },
+    methods:{
+      
+    }
+  }
+</script>
+<style scoped lang="scss">
+  *{
+    outline: 1px red dotted;
+  }
+  .card{
+    flex:1 1 auto;
+    padding:2rem;
+  }
+  #button-group>.btn{
+    flex:0 1 auto;
+  }
+  .info :nth-child(1){
+    border-bottom-width:1rem;
+    border-color:#f0ad4e;
+  }
+  .info :nth-child(2){
+    border-bottom-width:1rem;
+    border-color:#5bc0de;
+  }
+  .info :nth-child(3){
+    border-bottom-width:1rem;
+  }
+</style>
---
+++ resources/js/pages/finance/financeSidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!--只有組別-->
+    <div v-for="(team,index) in teamList[`${filterYear}`]" :key="`ary${index}`" v-if="isArray==true" class="aryClass h4">{{team}}
+    </div>
+    <!--部+組-->
+    <div v-for="(team,index) in teamList[`${filterYear}`]" :key="`obj${index}`" v-if="isArray==false" class="objClass h4">{{index}}
+      <div v-for="(group,grdex) in team" :key="grdex" class="groupClass ml-3">{{group}}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default{
+  props:['filterYear'],
+  data(){
+    return{
+      teamList:{
+          "2021":{
+            "行政部":["活動組","議程組"],
+            "產品部":["研發組","設計組","行銷組"],
+            "商務部":["商業開發組","公關組","媒體組"],
+          },
+          "2022":["aa","bb","dd"]
+      },
+      isArray:false,
+    }
+  },
+  watch:{
+    filterYear:'currentTeamList'
+  },
+  methods:{
+    currentTeamList:function(year){
+      const vm = this;
+      year = vm.filterYear;
+      if(vm.teamList[year].constructor === Array){
+        vm.isArray=true;
+      }else{
+        vm.isArray=false;
+      }
+    }
+  },
+}
+</script>
+<style scoped lang="scss">
+.aryClass, .objClass, .groupClass{
+  line-height: 2;
+}
+.groupClass{
+  font-size: 1.2rem;
+}
+</style>
